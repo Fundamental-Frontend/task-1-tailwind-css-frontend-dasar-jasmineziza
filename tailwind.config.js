10c5307f37f9e865c2539310b0c515ffcb9d6e5a
jasmineziza <justmine.jasmine.123@gmail.com>
2026-03-09 23:41:16 +0700

    5053231003_JasmineAziza_Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563EB',
+        secondary: '#16A34A',
+        background: '#F8FAFC',
+      },
+    },
+  },
+  plugins: [],
+};
